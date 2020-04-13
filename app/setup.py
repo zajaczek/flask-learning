31f859b9c4a1317243bce313f51b9ce611cc23ff
Krzysztof Królikowski <krzysztof.krolikowski@interia.pl>
2020-04-13 20:56:00 +0200

    initial commit

---
+++ app/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='flask-tutorial',
+    version='0.0.1',
+    packages=[''],
+    package_dir={'': 'app'},
+    url='',
+    license='',
+    author='KK',
+    author_email='',
+    description=''
+)
