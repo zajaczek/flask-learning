4430c0f24d2d640bf9323b335c050e3a0042184e
Krzysztof Królikowski <krzysztof.krolikowski@interia.pl>
2020-04-13 21:34:23 +0200

    migration

--- app/routes.py
+++ app/routes.py
@@ -26,16 +26,6 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
-    return render_template(url_for('index'), title='Sign In', form=form)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
-
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         flash('Login requested for user {}, remember_me={}'.format(
-#             form.username.data, form.remember_me.data))
-#         return redirect('/index')
-#     return render_template('login.html', title='Sign In', form=form)
--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'I-like-cookies'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:/{os.path.join(basedir, "app.db")}'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or fr'sqlite:///{os.path.join(basedir, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
